fad8e27bb9ef074e1f3256bd54eba27b6b400a71
laple <laplepnv23@gmail.com>
2025-04-02 00:32:30 +0700

    feat: screen header

--- src/navigators/bottomTabNavigator.tsx
+++ src/navigators/bottomTabNavigator.tsx
@@ -1,13 +1,16 @@
-import { View, Text, SafeAreaView } from 'react-native';
-import { SvgProps } from 'react-native-svg';
 import React from 'react';
+import { SvgProps } from 'react-native-svg';
+import LinearGradient from 'react-native-linear-gradient';
+import { useNavigationState } from '@react-navigation/native';
+import { View, Text, StatusBar, TouchableOpacity } from 'react-native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 
 import Icon from '../components/Icon';
+import MenuIcon from '../assets/icons/menu.svg';
+import ScanIcon from '../assets/icons/scan.svg';
 
 import { styles } from './styles';
 import { BOTTOM_TAB_ROUTES, COLORS } from '../constants';
-import LinearGradient from 'react-native-linear-gradient';
 
 const Tab = createBottomTabNavigator();
 
@@ -24,45 +27,70 @@ const TabBarItem = ({ focused, svgComponent, label }: { focused: boolean; svgCom
     );
 };
 
-const CustomHeader = ({ title }: { title?: string }) => {
+const CustomHeader = ({ title, name }: { title?: string; name: string }) => {
     return (
-        <SafeAreaView>
-            <LinearGradient
-                colors={[COLORS.blue300, COLORS.blue500]}
-                start={{ x: 0, y: 0 }}
-                end={{ x: 0, y: 1 }}
-                style={styles.headerContainer}
-            >
-                <Text style={styles.headerTitle}>{title}</Text>
-            </LinearGradient>
-        </SafeAreaView>
+        <LinearGradient
+            colors={[COLORS.blue300, COLORS.blue500]}
+            start={{ x: 0, y: 0 }}
+            end={{ x: 0, y: 1 }}
+            style={styles.headerContainer}
+        >
+            {name === 'ProductionOrder' ? (
+                <TouchableOpacity style={styles.headerButtonStyle}>
+                    <Icon SVGComponent={MenuIcon} color={COLORS.gray500} size={32} />
+                </TouchableOpacity>
+            ) : <View />}
+            <Text style={styles.headerTitle}>{title}</Text>
+            {name === 'ProductionOrder' ? (
+                <TouchableOpacity style={styles.headerButtonStyle}>
+                    <Icon SVGComponent={ScanIcon} color={COLORS.gray500} size={24} />
+                </TouchableOpacity>
+            ) : <View />}
+        </LinearGradient>
     );
 };
 
-const BottomTabNavigator = () => {
-    return (
-        <Tab.Navigator
-            initialRouteName="Overview"
-            backBehavior='history'
-            screenOptions={{
-                headerShown: true,
-                tabBarShowLabel: false,
-                tabBarStyle: styles.tabbarContainer,
-            }}>
-            {
-                BOTTOM_TAB_ROUTES.map((item) => (
-                    <Tab.Screen key={item.name} name={item.name} component={item.component} options={{
-                        headerShown: item.headerLabel ? true : false,
-                        tabBarIcon: ({ focused }) => TabBarItem({ focused, label: item.label, svgComponent: focused ? item.svgComponentActive : item.svgComponent }),
-                        tabBarLabelPosition: 'beside-icon',
-                        headerTitle: item.headerLabel,
-                        headerStyle: styles.headerContainer,
-                        header: () => CustomHeader({ title: item.headerLabel }),
-                    }} />
-                ))
-            }
 
-        </Tab.Navigator>
+const BottomTabNavigator = () => {
+    const currentRouteName = useNavigationState((state) => {
+        const bottomTabRoutes = state.routes[state.index];
+        const route = bottomTabRoutes.state?.routes[bottomTabRoutes.state.index || 0];
+        return route?.name;
+    });
+
+    const currentRoute = BOTTOM_TAB_ROUTES.find((route) => route.name === currentRouteName);
+    const isHeaderLabelPresent = currentRoute?.headerLabel !== '';
+
+    return (
+        <View style={styles.container}>
+            <StatusBar
+                barStyle={isHeaderLabelPresent ? 'light-content' : 'dark-content'}
+                backgroundColor="transparent"
+                translucent={isHeaderLabelPresent}
+            />
+            <Tab.Navigator
+                initialRouteName="Overview"
+                backBehavior="history"
+                screenOptions={{
+                    headerShown: true,
+                    tabBarShowLabel: false,
+                    tabBarStyle: styles.tabbarContainer,
+                }}>
+                {
+                    BOTTOM_TAB_ROUTES.map((item) => (
+                        <Tab.Screen key={item.name} name={item.name} component={item.component} options={{
+                            headerShown: item.headerLabel ? true : false,
+                            tabBarIcon: ({ focused }) => TabBarItem({ focused, label: item.label, svgComponent: focused ? item.svgComponentActive : item.svgComponent }),
+                            tabBarLabelPosition: 'beside-icon',
+                            headerTitle: item.headerLabel,
+                            headerStyle: styles.headerContainer,
+                            header: () => CustomHeader({ title: item.headerLabel, name: item.name }),
+                        }} />
+                    ))
+                }
+
+            </Tab.Navigator>
+        </View>
     );
 };
 
--- src/navigators/styles.ts
+++ src/navigators/styles.ts
@@ -1,16 +1,28 @@
 import {Dimensions, Platform, StyleSheet} from 'react-native';
+
 import {COLORS} from '../constants/colors';
 
 const {width} = Dimensions.get('window');
 const isAndroid = Platform.OS === 'android';
 
 export const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
   headerContainer: {
-    backgroundColor: 'red',
+    height: isAndroid ? 88 : 98,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'flex-end',
   },
   headerTitle: {
     fontSize: 18,
     fontWeight: '500',
+    marginBottom: 10,
+    color: 'white',
+  },
+  headerButtonStyle: {
+    padding: 10,
   },
   tabbarContent: {
     alignItems: 'center',
